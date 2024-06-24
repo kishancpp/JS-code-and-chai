bfa984afe4b3ec870647a911a7fef57571adb555
kishan singh <122980427+kishancpp@users.noreply.github.com>
2024-06-24 08:14:17 +0000

    some changes in string

--- 01_basics/Strings.js
+++ 01_basics/Strings.js
@@ -7,6 +7,7 @@ const repoCount = 50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
+console.log(typeof gameName)
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
